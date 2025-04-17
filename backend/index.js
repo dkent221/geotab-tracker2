204b22c9de117628df16c076fef660a9d7735d01
David Kent <42719141+dkent221@users.noreply.github.com>
2025-04-17 10:01:51 -0700

    fix for render

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${5000}`);
+});
 
 app.use(cors());
 
